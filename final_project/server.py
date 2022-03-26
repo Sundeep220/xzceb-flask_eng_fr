a648c5e2d39dd3f8ec2d3b3aaa958066542b247c
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-03-27 03:47:34 +0530

    This is last change

--- final_project/server.py
+++ final_project/server.py
@@ -2,21 +2,21 @@ from cgitb import text
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-from machinetranslation import translator
+from machinetranslation import translator,tests
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    translator.english_to_french(textToTranslate)
-    return "Translated text to French"
+    french=translator.english_to_french(textToTranslate)
+    return french
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    translator.french_to_english(textToTranslate)
-    return "Translated text to English"
+    english=translator.french_to_english(textToTranslate)
+    return english
 
 @app.route("/")
 def renderIndexPage():
